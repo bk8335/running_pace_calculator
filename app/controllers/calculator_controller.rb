752a5daf971d023e1d18274ef9b96c69d792fe49
bk8335 <bk8335@gmail.com>
2017-11-23 23:16:21 +0000

    pace in mins per km, incline added to calc

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -8,13 +8,18 @@ class CalculatorController < ApplicationController
 
   def results
   	# debugger
-  	@distance = params[:distance].to_i
+  	@distance = params[:distance].to_d
   	@hours = params[:hours].to_i
   	@minutes = params[:minutes].to_i
   	@seconds = params[:seconds].to_i
+  	@incline = params[:incline].to_d
+
   	@time_in_secs = (@hours * 3600) + (@minutes * 60) + @seconds
 
-  	@pace = ((@distance.to_d / @time_in_secs) * 3600).ceil(1)
+  	@pace_hour = ((@distance.to_d / @time_in_secs) * 3600).ceil(1)
+  	@elevation = ( @incline / 100 ) * @distance * 1000
+  	@pace_km_min = (60 / @pace_hour).floor
+  	@pace_km_sec = ((60 / @pace_hour) - @pace_km_min ) * 60
   	@pace_round = ((@distance.to_d / @time_in_secs) * 3600).round(3)
   end
 end
