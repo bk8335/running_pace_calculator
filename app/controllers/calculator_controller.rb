80b815019518ee1c03fed07a1915d70caa1b3c82
bk8335 <bk8335@gmail.com>
2017-11-24 08:21:25 +0000

    form also renders on results page. Units appear but not yet working

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -3,7 +3,6 @@ class CalculatorController < ApplicationController
   end
 
   def calculate_pace
-  
   end
 
   def results
@@ -15,11 +14,10 @@ class CalculatorController < ApplicationController
   	@incline = params[:incline].to_d
 
   	@time_in_secs = (@hours * 3600) + (@minutes * 60) + @seconds
-
   	@pace_hour = ((@distance.to_d / @time_in_secs) * 3600).ceil(1)
   	@elevation = ( @incline / 100 ) * @distance * 1000
   	@pace_km_min = (60 / @pace_hour).floor
-  	@pace_km_sec = ((60 / @pace_hour) - @pace_km_min ) * 60
+  	@pace_km_sec = (((60 / @pace_hour) - @pace_km_min ) * 60).floor
   	@pace_round = ((@distance.to_d / @time_in_secs) * 3600).round(3)
   end
 end
